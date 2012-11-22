4473b4b18923a410efea916e35171bf4ced148ac
Daneel S. Yaitskov <dy@hivext.net>
2012-11-22 13:59:57 +0400

    mysql for development env; generator books if none

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import org.example.Book
+
 class BootStrap {
 
     def init = { servletContext ->
+        // check whether test already exists
+        if (!Book.count()) {
+            new Book(author: "Stephen King", title: "Hello world").save(failOnError: true)
+            new Book(author: "Gang of Four", title: "Design Patterns").save(failOnError: true)
+        }
     }
     def destroy = {
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,8 +13,11 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "update"
+            driverClassName = "com.mysql.jdbc.Driver"
+            url = "jdbc:mysql://localhost/grails1"
+            username = "root"
+            password = "1"
         }
     }
     test {
@@ -29,14 +32,14 @@ environments {
             url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
             pooled = true
             properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
+                maxActive = -1
+                minEvictableIdleTimeMillis = 1800000
+                timeBetweenEvictionRunsMillis = 1800000
+                numTestsPerEvictionRun = 3
+                testOnBorrow = true
+                testWhileIdle = true
+                testOnReturn = true
+                validationQuery = "SELECT 1"
             }
         }
     }
